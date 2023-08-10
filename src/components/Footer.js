cb023b2216045cec6112e31e7288abfce1ff1cad
Nekojar <mzharfanhakim7@gmail.com>
2023-08-10 22:04:50 +0700

    adding some links into the footers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,25 @@ export function Footer() {
   return (
     <div className="footer">
       <div className="social-media">
-        <p>Instagram </p>
-        <p>Facebook </p>
-        <p>Twitter </p>
-        <p>About Us</p>
+        <a href="https://twitter.com/kingdomhearts" target="blank">
+          Twitter{" "}
+        </a>
+        <a href="https://www.kingdomhearts.com/home/us/" target="blank">
+          About Us
+        </a>
       </div>
-      <span>2002-2023 ©️Disney.</span>
+      <div>
+        <span className="social-media">
+          © 2002-2023{" "}
+          <a href="https://www.disney.com/" target="blank">
+            Disney
+          </a>
+          © 2002-2023
+          <a href="https://www.square-enix-games.com/" target="blank">
+            Square Enix.
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
